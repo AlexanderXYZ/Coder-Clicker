094cf9aad897de0b9b137dc6eb45a282b96f6d0c
Alexander <79529341506@yandex.ru>
2021-07-19 10:39:12 +0700

    setup navigation

--- app/src/main/java/com/buslaev/coderclicker/MainActivity.kt
+++ app/src/main/java/com/buslaev/coderclicker/MainActivity.kt
@@ -2,10 +2,32 @@ package com.buslaev.coderclicker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        navController = findNavController(R.id.fragmentContainerView)
+        bottom_nav_view.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            when (destination.id) {
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
